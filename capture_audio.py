377379b1d08009d5480af0eae7d60e9bbeaea51e
Ben Krikler <bek07@ic.ac.uk>
2019-10-11 20:48:59 +0200

    Actually try recording something (despite the devices not appearing)

--- capture_audio.py
+++ capture_audio.py
@@ -1,5 +1,46 @@
 import pyaudio
 import pprint
 p = pyaudio.PyAudio()
-device = [p.get_device_info_by_index(i) for i in range(p.get_device_count())]
-pprint.pprint(device)
+devices = [p.get_device_info_by_index(i) for i in range(p.get_device_count())]
+outputs = devices
+#outputs = [d for d in devices if d["maxInputChannels"] > 0]
+pprint.pprint(outputs)
+print("Number of outputs", len(outputs))
+
+import wave
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
