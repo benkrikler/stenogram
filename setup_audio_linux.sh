665c673ec41f36dddedf6067c87597e9b633c34f
Ben Krikler <bek07@ic.ac.uk>
2019-10-11 20:48:51 +0200

    Add device descriptions

--- setup_audio_linux.sh
+++ setup_audio_linux.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 ScriptDir="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 
-mod_dupl=$(pactl load-module module-null-sink sink_name=duplex_out)
-mod_web_conf=$(pactl load-module module-null-sink sink_name=web_conf_out)
+mod_dupl=$(pactl load-module module-null-sink sink_name=duplex_out sink_properties=device.description="Duplex_Output")
+mod_web_conf=$(pactl load-module module-null-sink sink_name=web_conf_out sink_properties=device.description="Web_Conf_output")
 mod_loop_conf=$(pactl load-module module-loopback source=web_conf_out.monitor)
 mod_loop_rec=$(pactl load-module module-loopback source=web_conf_out.monitor sink=duplex_out)
 mod_loop_out=$(pactl load-module module-loopback sink=duplex_out)
